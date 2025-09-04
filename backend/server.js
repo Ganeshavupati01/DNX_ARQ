b5e633bf5331a5f627b19cce2af62e8794980524
Avupati Ganesh <23pa5a0501@vishnu.edu.in>
2025-09-05 00:06:08 +0530

    second1

--- backend/server.js
+++ backend/server.js
@@ -56,5 +56,9 @@ app.put('/tasks/:id/progress', async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend API is running.");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
